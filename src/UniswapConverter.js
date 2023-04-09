6bd191a13c414fb3ecc52fc222b845fdc09491f9
dkirsche <dan.kirsche@gmail.com>
2023-04-09 18:31:24 +0000

    Added the ability to calculate gas costs for uniswap.

--- src/UniswapConverter.js
+++ src/UniswapConverter.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { ChainId, Token, WETH, Route, TokenAmount, Fetcher, Trade, TradeType } from '@uniswap/sdk';
+import { ethers } from 'ethers';
+import axios from 'axios';
 
 const UniswapConverter = () => {
-  // ...
-
   const [uniswapConversionRate, setUniswapConversionRate] = useState(null);
-
-  // ...
+  const [uniswapGasFee, setUniswapGasFee] = useState(null);
 
   useEffect(() => {
     const fetchUniswapConversionRate = async () => {
@@ -23,18 +22,41 @@ const UniswapConverter = () => {
       }
     };
 
-    fetchUniswapConversionRate();
+    const estimateUniswapGasFee = async () => {
+      try {
+        const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/c3211f935cc24cbaa35e33b66930e06d');
+        const gasPrice = await provider.getGasPrice();
+        const ethPriceResponse = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd');
+        const ethPrice = ethPriceResponse.data.ethereum.usd;
+        const estimatedGas = 21000; // Approximate gas usage for a Uniswap swap
+        const gasFeeInETH = ethers.utils.formatEther(gasPrice.mul(estimatedGas));
+        const gasFeeInUSD = (parseFloat(gasFeeInETH) * ethPrice).toFixed(2);
+        setUniswapGasFee(gasFeeInUSD);
+      } catch (error) {
+        console.error('Error estimating Uniswap gas fee:', error);
+      }
+    };
+
+    fetchUniswapConversionRate().then(() => {
+      estimateUniswapGasFee();
+    });
   }, []);
 
   return (
     <div>
-      { /* ... */ }
       {uniswapConversionRate ? (
         <p>
           Uniswap: <strong>{uniswapConversionRate}</strong>
         </p>
       ) : (
         <p>Loading Uniswap conversion rate...</p>
+      )}
+      {uniswapGasFee ? (
+        <p>
+          Uniswap gas fee (USD): <strong>{uniswapGasFee}</strong>
+        </p>
+      ) : (
+        <p>Loading Uniswap gas fee...</p>
       )}
     </div>
   );
